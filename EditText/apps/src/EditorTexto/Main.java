27958a9bf5ea765e3bdfc6b2ad4152739fb31f48
fdlang <purriaky@hotmail.com>
2022-12-19 03:08:18 +0100

    act

---
+++ EditText/apps/src/EditorTexto/Main.java
@@ -0,0 +1,12 @@
+package EditorTexto;
+
+
+public class Main {
+
+    public static void main(String[] args) {
+       VistaPrincipal app = new VistaPrincipal();       
+       app.frame();
+             
+
+    }
+}
---
+++ EditText/apps/src/EditorTexto/VistaPrincipal.java
@@ -0,0 +1,30 @@
+package EditorTexto;
+
+import java.awt.BorderLayout;
+
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.JPanel;
+import javax.swing.JTextArea;
+
+public class VistaPrincipal extends JFrame{
+    static JTextArea textoArea = new JTextArea();
+    
+    public void frame() {
+        setTitle("Editor de Texto");
+        setSize(500,700);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        
+        JPanel principal = new JPanel(); 
+        
+        principal.setLayout(new BorderLayout());
+        principal.add(textoArea, BorderLayout.CENTER);                
+        principal.add(new vistaMenu(), BorderLayout.NORTH);    
+               
+        setContentPane(principal);
+        setVisible(true); 
+    } 
+}
